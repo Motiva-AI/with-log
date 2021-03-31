5d99581628339aa4f718b5c1729785d597eba641
Paul Lam <paul@quantisan.com>
2021-04-01 08:03:58 +0900

    first arg should only be a message

--- src/with_log/core.clj
+++ src/with_log/core.clj
@@ -1,24 +1,17 @@
 (ns with-log.core
   (:require [clojure.tools.logging]))
 
-(defmacro with-log [message-or-config & body]
-  (if (map? message-or-config)
-    ;; Allow passing of some metric info
-    (let [{:keys [message]}
-          message-or-config]
-      `(with-log ~message
-         ~@body))
-
-    `(try
-       (clojure.tools.logging/infof "BEGIN: %s" ~message-or-config)
-       (let [ret# (do ~@body)]
-         (clojure.tools.logging/infof "END: %s" ~message-or-config)
-         ret#)
-       (catch Throwable tw#
-         (let [ex-data# (if-let [ex-data# (ex-data tw#)]
-                          (str "ex-data=" ex-data#)
-                          (str "no ex-data available"))]
-           (clojure.tools.logging/warnf tw# "FAILED: %s. %s" ~message-or-config ex-data#))
-         ;; Rethrow for recovery
-         (throw tw#)))))
+(defmacro with-log [message & body]
+  `(try
+     (clojure.tools.logging/infof "BEGIN: %s" ~message)
+     (let [ret# (do ~@body)]
+       (clojure.tools.logging/infof "END: %s" ~message)
+       ret#)
+     (catch Throwable tw#
+       (let [ex-data# (if-let [ex-data# (ex-data tw#)]
+                        (str "ex-data=" ex-data#)
+                        (str "no ex-data available"))]
+         (clojure.tools.logging/warnf tw# "FAILED: %s. %s" ~message ex-data#))
+       ;; Rethrow for recovery
+       (throw tw#))))
 
