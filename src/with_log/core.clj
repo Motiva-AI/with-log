d30449997113f88396d36f79f3abe3f7d785550a
Paul Lam <paul@quantisan.com>
2021-04-01 08:29:35 +0900

    refactor out elapsed-time fn

--- src/with_log/core.clj
+++ src/with_log/core.clj
@@ -2,18 +2,19 @@
   (:require [clojure.tools.logging]))
 
 (defmacro with-log [message & body]
-  `(let [start# (. System (nanoTime))]
+  `(let [start#        (. System (nanoTime))
+         ;; borrowed from (clojure.core/time)
+         elapsed-time# #(int (/ (double (- (. System (nanoTime)) %)) 1000000.0))]
      (try
        (clojure.tools.logging/infof "BEGIN: %s" ~message)
-
        (let [ret# (do ~@body)]
-
          (clojure.tools.logging/infof
            "END (elapsed time [%d] msecs): %s"
-           (int (/ (double (- (. System (nanoTime)) start#)) 1000000.0)) ;; borrowed from (clojure.core/time)
+           (elapsed-time# start#)
            ~message)
 
          ret#)
+
        (catch Throwable tw#
          (let [ex-data# (if-let [ex-data# (ex-data tw#)]
                           (str "ex-data=" ex-data#)
@@ -21,9 +22,10 @@
            (clojure.tools.logging/warnf
              tw#
              "FAILED (elapsed time [%d] msecs): %s. %s"
-             (int (/ (double (- (. System (nanoTime)) start#)) 1000000.0)) ;; borrowed from (clojure.core/time)
+             (elapsed-time# start#)
              ~message
              ex-data#))
+
          ;; Rethrow for recovery
          (throw tw#)))))
 
