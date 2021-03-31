5f383bd7417950e6b287946e6a86ef547b7cf6b2
Paul Lam <paul@quantisan.com>
2021-04-01 07:53:11 +0900

    print elapsed time at END message

--- src/with_log/core.clj
+++ src/with_log/core.clj
@@ -2,16 +2,28 @@
   (:require [clojure.tools.logging]))
 
 (defmacro with-log [message & body]
-  `(try
-     (clojure.tools.logging/infof "BEGIN: %s" ~message)
-     (let [ret# (do ~@body)]
-       (clojure.tools.logging/infof "END: %s" ~message)
-       ret#)
-     (catch Throwable tw#
-       (let [ex-data# (if-let [ex-data# (ex-data tw#)]
-                        (str "ex-data=" ex-data#)
-                        (str "no ex-data available"))]
-         (clojure.tools.logging/warnf tw# "FAILED: %s. %s" ~message ex-data#))
-       ;; Rethrow for recovery
-       (throw tw#))))
+  `(let [start# (. System (nanoTime))]
+     (try
+       (clojure.tools.logging/infof "BEGIN: %s" ~message)
+
+       (let [ret# (do ~@body)]
+
+         (clojure.tools.logging/infof
+           "END (elapsed time [%d] msecs): %s"
+           (int (/ (double (- (. System (nanoTime)) start#)) 1000000.0)) ;; borrowed from (clojure.core/time)
+           ~message)
+
+         ret#)
+       (catch Throwable tw#
+         (let [ex-data# (if-let [ex-data# (ex-data tw#)]
+                          (str "ex-data=" ex-data#)
+                          (str "no ex-data available"))]
+           (clojure.tools.logging/warnf
+             tw#
+             "FAILED (elapsed time [%d] msecs): %s. %s"
+             (int (/ (double (- (. System (nanoTime)) start#)) 1000000.0)) ;; borrowed from (clojure.core/time)
+             ~message
+             ex-data#))
+         ;; Rethrow for recovery
+         (throw tw#)))))
 
