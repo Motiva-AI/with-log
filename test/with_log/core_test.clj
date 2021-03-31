c9e14b69843dd48525b1277676c8ce30eeec4320
Paul Lam <paul@quantisan.com>
2021-04-01 07:29:35 +0900

    add core-test

---
+++ test/with_log/core_test.clj
@@ -0,0 +1,26 @@
+(ns with-log.core-test
+  (:require [clojure.test :refer :all]
+            [clojure.tools.logging.test :as log.test]
+            [with-log.core :refer [with-log]]))
+
+(deftest with-log-test
+  (let [msg "test message 123"]
+    (testing "happy path"
+      (log.test/with-log
+        (is (= :foo (with-log msg :foo)))
+
+        (is (log.test/logged? 'with-log.core-test :info (str "BEGIN: " msg)))
+        (is (log.test/logged? 'with-log.core-test :info (str "END: " msg)))))
+
+    (testing "failed path"
+      (let [fail-message "testing fail case"
+            e            (Exception. fail-message)]
+        (log.test/with-log
+          (is (thrown-with-msg?
+                Exception
+                (re-pattern fail-message)
+                (with-log msg ((fn [] (throw e))))))
+
+          (is (log.test/logged? 'with-log.core-test :info (str "BEGIN: " msg)))
+          (is (log.test/logged? 'with-log.core-test :warn e (re-pattern (str "FAILED: " msg)))))))))
+
